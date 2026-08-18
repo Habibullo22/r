4905055d35752c6dcb623964aadfbdddeebc4018
Habibullo22 <habibulloabidjonov555@gmail.com>
2026-08-18 09:25:41 +0500

    Update telegram_client.py

--- telegram_client.py
+++ telegram_client.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from telethon import TelegramClient
 from config import API_ID, API_HASH
 
@@ -5,11 +7,45 @@ from config import API_ID, API_HASH
 SESSION_NAME = "telegram_account"
 
 
-def create_client():
-    client = TelegramClient(
-        SESSION_NAME,
-        API_ID,
-        API_HASH
+# Bitta doimiy event loop
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+
+# Telegram client
+telegram_client = TelegramClient(
+    SESSION_NAME,
+    API_ID,
+    API_HASH,
+    loop=loop
+)
+
+
+def get_client():
+    return telegram_client
+
+
+def start_client():
+    """
+    Telegram akkauntini ulaydi.
+    """
+    loop.run_until_complete(
+        telegram_client.start()
     )
 
-    return client
+
+def get_me():
+    """
+    Ulangan akkaunt ma'lumotlarini oladi.
+    """
+    return loop.run_until_complete(
+        telegram_client.get_me()
+    )
+
+
+def run_async(coro):
+    """
+    Barcha Telegram async funksiyalarini
+    bir xil event loop orqali ishlatadi.
+    """
+    return loop.run_until_complete(coro)
