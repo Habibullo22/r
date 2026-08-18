49618c50bec59112ffb6acadf3bbfe4ba89838da
Habibullo22 <habibulloabidjonov555@gmail.com>
2026-08-18 07:48:05 +0500

    Initialize config.py with environment variables

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
